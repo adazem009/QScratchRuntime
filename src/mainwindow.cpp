2c88214f4701a90363c13b19b34745827252f12a
adazem009 <68537469+adazem009@users.noreply.github.com>
2022-01-03 11:08:07 +0100

    Update copyright year of changed files

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,7 +2,7 @@
  * mainwindow.cpp
  * This file is part of QScratchRuntime
  *
- * Copyright (C) 2021 - adazem009
+ * Copyright (C) 2021-2022 - adazem009
  *
  * QScratchRuntime is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
