c6dac12a13cd5133e5429a5da664f38935190d7d
adazem009 <adazem009@users.noreply.github.com>
2022-01-05 12:20:40 +0000

    deploy: 75a0c6f6a56c0fd565dcc002369f77f37ffc395b

--- search/functions_8.js
+++ search/functions_8.js
@@ -4,11 +4,12 @@ var searchData=
   ['setcostume_1',['setCostume',['../classscratchSprite.html#a508ac9693e4f1ae523b4316fd9067833',1,'scratchSprite']]],
   ['setdirection_2',['setDirection',['../classscratchSprite.html#a7ba47275426e6925d8e37d9ecb84e349',1,'scratchSprite']]],
   ['setmousepos_3',['setMousePos',['../classscratchSprite.html#a0f00f9ca722b62c15b74a3fee89f561a',1,'scratchSprite']]],
-  ['setxpos_4',['setXPos',['../classscratchSprite.html#ade994e8c0803e447564e92545d69c8a8',1,'scratchSprite']]],
-  ['setypos_5',['setYPos',['../classscratchSprite.html#ac3d432973f42391618ef53af03631301',1,'scratchSprite']]],
-  ['showbubble_6',['showBubble',['../classscratchSprite.html#aa6d1bc1e75fcb29a8a70ff561a7039b7',1,'scratchSprite']]],
-  ['sprites_7',['sprites',['../classprojectParser.html#ab4d4e9b006743666101d500a4c74608b',1,'projectParser']]],
-  ['stage_8',['stage',['../classprojectParser.html#a652bca50042e3109eb9b7dfbab7d953e',1,'projectParser']]],
-  ['stopscripts_9',['stopScripts',['../classscratchSprite.html#a050df00d3d24f6cb179431f9c1c1257c',1,'scratchSprite']]],
-  ['stopsprite_10',['stopSprite',['../classscratchSprite.html#ad9ec8bf8179adb85bf448b1b285e5e94',1,'scratchSprite']]]
+  ['setsize_4',['setSize',['../classscratchSprite.html#aaa192f3c27395177029a47366ee143d8',1,'scratchSprite']]],
+  ['setxpos_5',['setXPos',['../classscratchSprite.html#ade994e8c0803e447564e92545d69c8a8',1,'scratchSprite']]],
+  ['setypos_6',['setYPos',['../classscratchSprite.html#ac3d432973f42391618ef53af03631301',1,'scratchSprite']]],
+  ['showbubble_7',['showBubble',['../classscratchSprite.html#aa6d1bc1e75fcb29a8a70ff561a7039b7',1,'scratchSprite']]],
+  ['sprites_8',['sprites',['../classprojectParser.html#ab4d4e9b006743666101d500a4c74608b',1,'projectParser']]],
+  ['stage_9',['stage',['../classprojectParser.html#a652bca50042e3109eb9b7dfbab7d953e',1,'projectParser']]],
+  ['stopscripts_10',['stopScripts',['../classscratchSprite.html#a050df00d3d24f6cb179431f9c1c1257c',1,'scratchSprite']]],
+  ['stopsprite_11',['stopSprite',['../classscratchSprite.html#ad9ec8bf8179adb85bf448b1b285e5e94',1,'scratchSprite']]]
 ];
