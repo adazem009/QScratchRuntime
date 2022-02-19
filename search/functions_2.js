8b3806e17798e5867877ed5d09ec0f01a4456886
adazem009 <adazem009@users.noreply.github.com>
2022-02-19 08:33:16 +0000

    deploy: 86656cdc32495aaea906f103549bfc185b3a6806

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['checkkey_0',['checkKey',['../classscratchSprite.html#aa41f796acf1b9c95ece39e28353c252e',1,'scratchSprite']]],
-  ['continueloading_1',['continueLoading',['../classMainWindow.html#a72b0f5ec864d378156f19836d57b2033',1,'MainWindow']]],
-  ['controlblocks_2',['controlBlocks',['../classscratchSprite.html#a8bf8391d97d27320e0e28c160c9a0bbb',1,'scratchSprite']]]
+  ['clearspritelist_1',['clearSpriteList',['../classprojectScene.html#a00167146ef77ffbd8975476bd45ae662',1,'projectScene']]],
+  ['continueloading_2',['continueLoading',['../classMainWindow.html#a72b0f5ec864d378156f19836d57b2033',1,'MainWindow']]],
+  ['controlblocks_3',['controlBlocks',['../classscratchSprite.html#a8bf8391d97d27320e0e28c160c9a0bbb',1,'scratchSprite']]]
 ];
